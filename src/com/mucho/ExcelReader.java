3c0a3fa63ecc2b6ac5b189d0a598bd4c14c98a2a
njherrera <nathan.j.herrera@gmail.com>
2021-12-18 23:32:54 -0800

    added outline for buildCareers, the method that will be used to process the data sets

--- src/com/mucho/ExcelReader.java
+++ src/com/mucho/ExcelReader.java
@@ -3,4 +3,12 @@ package com.mucho;
 // creates player objects from data set (probably excel)
 public class ExcelReader {
 
+
+    /* buildCareers (dataset) method
+        some kind of for each loop that goes through dataset row by row
+          if season is not included in an existing player's career, either add it to an existing player's career or use it as the first season of a new player's career
+
+        HashMap to hold player's catalogued so far?
+            checking to see if a player's already been created would just be checking to see if HashMap.get(player name) returns null
+     */
 }
