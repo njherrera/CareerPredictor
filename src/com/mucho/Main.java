c0607ecdf74825e27d88a1206721d0ab311bea69
njherrera <nathan.j.herrera@gmail.com>
2022-01-05 18:58:39 -0800

    finished making the MySQL database, now time to start writing code

--- src/com/mucho/Main.java
+++ src/com/mucho/Main.java
@@ -3,8 +3,13 @@ package com.mucho;
 public class Main {
 
     /*
-    Decide on data sets
-    Make excel doc with 10 players most similar to given player
+    X Decide on data sets
+    X set up MySQL database for players and seasons
+    Write code for comparing players
+    Write code for generating a composite player based on most similar players and their similarity scores
+        i.e. weight players with similarity score of 70 more than players with score of 50
+    Write code for using comparisons to make projections
+    Select 10 players from dataset that 538 identifies as most similar to given player
     Analyze 10 players, given player
     Run comparison on given player with just those 10 players
     Check comparison vs 538
